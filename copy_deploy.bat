d3dae7cbcca3cfbdfc34f6f260e2f11a341a7fc1
Adam Majcherek <dem0n1358v2@gmail.com>
2019-08-04 18:47:09 +0200

    oops

--- copy_deploy.bat
+++ copy_deploy.bat
@@ -2,5 +2,5 @@
 echo "Moving build content..."
 robocopy Assets\Plugins\NLua Deploy\NLua /S /xf *.meta
 robocopy Library\ScriptAssemblies Deploy /S SimpleLUI.dll SimpleLUI.dll.pdb SimpleLUI.Editor.dll SimpleLUI.Editor.dll.pdb
-robocopy Library\ScriptAssemblies Deploy /S SimpleLUI.JEM.dll SimpleLUI.JEM.dll.pdb SimpleLUI.Editor.JEM.dll SimpleLUI.JEM.Editor.dll.pdb
+robocopy Library\ScriptAssemblies Deploy /S SimpleLUI.JEM.dll SimpleLUI.JEM.dll.pdb SimpleLUI.JEM.Editor.dll SimpleLUI.JEM.Editor.dll.pdb
 echo "Build done!"
